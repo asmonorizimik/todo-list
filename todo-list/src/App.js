c44882b95bac67ecaf31c59d55a788ddd92bd6a4
manory zimik <95528408+asmonorizimik@users.noreply.github.com>
2022-12-11 13:25:53 +0530

    first commit

---
+++ todo-list/src/App.js
@@ -0,0 +1,56 @@
+
+import App from './App.css'
+import React, {useState} from 'react';
+export default function Todo(){
+    const [list, setList] = useState([]);
+    const [allList, setAllList] = useState([]);
+
+    return (
+      <div id="main-container"> 
+      <h1>Make your list.....</h1>
+        Enter your items: <input type="text" value={list}  placeholder="enter..." onChange={(i)=>{
+          setList (i.target.value)
+        }}/>
+        <button id="submitBtn" onClick={()=>{
+          if (list.trim().length>0){
+            setAllList([...allList,list])
+          }
+          setList("")
+          
+        }}>add item
+        </button>
+
+        {
+          allList.map((item,index)=>{
+            return(
+              <div id="container">
+                <div >
+                <button id="slNo">{index+1}.</button>   <button id="item">{item.toUpperCase()}</button>
+                </div>
+            
+                <button onClick={()=>{
+                  let newData=[...allList]
+                  newData.splice(index,1)
+                  setAllList(newData)
+                }}>delete</button>
+
+                {/* EDIT */}
+
+                <button onClick={()=>{
+                  let newArr=[...allList]
+                  let newValue=prompt("enter....")
+                  if (newValue.trim().length>0){
+                    newArr[index]=newValue
+                  }
+                  setAllList(newArr)
+                }}>edit</button>
+                <button onClick={()=>{
+                  
+                }}>complete</button>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
